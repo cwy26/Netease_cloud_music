93abb0ea02e37c66cc16fab8ed858efc62026398
Harvey <52202752+cwy26@users.noreply.github.com>
2019-08-03 17:48:34 +0800

    添加了部分顶部导航条样式

---
+++ netcmusic/src/components/common/TitleBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="page-head">
+        <span class="find-listen"></span>
+        <div>
+            
+        </div>
+        <span></span>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    .page-head {
+        display: flex;
+        position: fixed;
+        z-index: 999;
+        width: 100%;
+        justify-content: space-between;
+        align-items: center;
+        background-color: aliceblue;
+        padding-right: 10px;
+        height: 50px;
+        color: #666;
+        font-size: 18px;
+    }
+    .page-head span {
+        width: 50px;
+        height: 50px;
+    }
+    .find-listen {
+        background: url('../../assets/icon/normal_32px/listen.png') center center no-repeat;
+    }
+</style>
+
--- netcmusic/src/views/xds_index.vue
+++ netcmusic/src/views/xds_index.vue
@@ -1,37 +1,54 @@
 <template>
-  <div>
-    <div class="page-wrap"></div>
-    <mt-tabbar v-model="active" fixed>
+  <div class="page-tabbar">
+    <div class="page-wrap">
+      <mt-tab-container v-model="active">
+        <mt-tab-container-item id="发现">
+          <titlebar></titlebar>  
+        </mt-tab-container-item>
+      </mt-tab-container>
+      <mt-tabbar v-model="active" fixed>
         <mt-tab-item id="发现" @click.native="changeState(0)">
-            <tabbaricon 
-            :selectedImage="require('./../assets/icon/chosen_32px/wangyiyunyinlezizhi-copy.png') " :normalImage="require('./../assets/icon/normal_32px/wangyiyunyinlezizhi-copy.png')"
-            :focused="currentIndex[0].isSelect"></tabbaricon>发现
+          <tabbaricon
+            :selectedImage="require('./../assets/icon/chosen_32px/wangyiyunyinlezizhi-copy.png') "
+            :normalImage="require('./../assets/icon/normal_32px/wangyiyunyinlezizhi-copy.png')"
+            :focused="currentIndex[0].isSelect"
+          ></tabbaricon>发现
         </mt-tab-item>
         <mt-tab-item id="视频" @click.native="changeState(1)">
-            <tabbaricon 
-            :selectedImage="require('./../assets/icon/chosen_32px/bofang.png') " :normalImage="require('./../assets/icon/normal_32px/bofang1.png')"
-            :focused="currentIndex[1].isSelect"></tabbaricon>视频
+          <tabbaricon
+            :selectedImage="require('./../assets/icon/chosen_32px/bofang.png') "
+            :normalImage="require('./../assets/icon/normal_32px/bofang1.png')"
+            :focused="currentIndex[1].isSelect"
+          ></tabbaricon>视频
         </mt-tab-item>
         <mt-tab-item id="我的" @click.native="changeState(2)">
-            <tabbaricon 
-            :selectedImage="require('./../assets/icon/chosen_32px/gedanshouluON.png') " :normalImage="require('./../assets/icon/normal_32px/gedanshouluON.png')"
-            :focused="currentIndex[2].isSelect"></tabbaricon>我的
+          <tabbaricon
+            :selectedImage="require('./../assets/icon/chosen_32px/gedanshouluON.png') "
+            :normalImage="require('./../assets/icon/normal_32px/gedanshouluON.png')"
+            :focused="currentIndex[2].isSelect"
+          ></tabbaricon>我的
         </mt-tab-item>
         <mt-tab-item id="云村" @click.native="changeState(3)">
-            <tabbaricon 
-            :selectedImage="require('./../assets/icon/chosen_32px/pengyou.png') " :normalImage="require('./../assets/icon/normal_32px/pengyou.png')"
-            :focused="currentIndex[3].isSelect"></tabbaricon>云村
+          <tabbaricon
+            :selectedImage="require('./../assets/icon/chosen_32px/pengyou.png') "
+            :normalImage="require('./../assets/icon/normal_32px/pengyou.png')"
+            :focused="currentIndex[3].isSelect"
+          ></tabbaricon>云村
         </mt-tab-item>
         <mt-tab-item id="账号" @click.native="changeState(4)">
-            <tabbaricon 
-            :selectedImage="require('./../assets/icon/chosen_32px/zhanghao.png') " :normalImage="require('./../assets/icon/normal_32px/zhanghao.png')"
-            :focused="currentIndex[4].isSelect"></tabbaricon>账号
+          <tabbaricon
+            :selectedImage="require('./../assets/icon/chosen_32px/zhanghao.png') "
+            :normalImage="require('./../assets/icon/normal_32px/zhanghao.png')"
+            :focused="currentIndex[4].isSelect"
+          ></tabbaricon>账号
         </mt-tab-item>
-    </mt-tabbar>
+      </mt-tabbar>
+    </div>
   </div>
 </template>
 <script>
 import TabBarIcon from "../components/common/TabBarIcon.vue";
+import TitleBar from "../components/common/TitleBar.vue";
 export default {
   data() {
     return {
@@ -51,22 +68,28 @@ export default {
       //对应数组值修改true其它修改false
       //1:创建循环,循环数组中内容
       for (var i of this.currentIndex) {
-          i.isSelect = false;
-        }
-        this.currentIndex[n].isSelect = true;
+        i.isSelect = false;
       }
+      this.currentIndex[n].isSelect = true;
+    }
   },
   components: {
-      tabbaricon: TabBarIcon
+    tabbaricon: TabBarIcon,
+    titlebar: TitleBar
   }
 };
 </script>
 <style scoped>
+/*最外层父元素index.vue*/
+.page-tabbar {
+  overflow: hidden; /*溢出隐藏*/
+}
 .page-wrap {
   width: 100%;
   height: 736px;
   background-color: var(--red); /* #d33a33 */
   overflow: auto; /*溢出显示轮动条*/
+  box-sizing: border-box;
   padding-bottom: 60px;
 }
 /*修改 tabbar 默认文字颜色*/

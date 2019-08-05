60a4ad9974612cdf3b5190e9b15112b33573757b
Harvey <52202752+cwy26@users.noreply.github.com>
2019-08-05 16:36:35 +0800

    沙雕张杰明你再用小写试试?

---
+++ netcmusic/src/components/common/NewProduct/song1.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+       <span>{{MoreSong}}</span>
+    </div>
+</template>
+<script>
+    export  default {
+        props:{
+           MoreSong:{default:""}
+       }
+    }
+</script>
+<style scoped>
+    span{
+        background: #ffffff;
+        border:1px solid #eee;
+        border-radius:15px;
+        padding: 5px  10px;
+        font-size: 10px;
+    }
+</style>
